ef1be984d29fffe8127f899060e6820e323729a3
Vicen <vilicaprogramer@gmail.com>
2026-03-08 23:24:15 +0100

    Documentación del código final

--- src/app.js
+++ src/app.js
@@ -3,19 +3,24 @@ const app = express();
 
 const path = require('path');
 
-// 1. Servir archivos estáticos
+// --- Configuración de Archivos Estáticos ---
+// Usamos path.join y __dirname para asegurar que la ruta a la carpeta 'public' 
+// sea correcta sin importar dónde se ejecute el código (local o nube).
 app.use(express.static(path.join(__dirname, '../public')));
 
-// 2. RUTA RAÍZ EXPLÍCITA (Esto es lo que te falta)
+// --- Ruta Raíz Explícita ---
+// Le decimos a Express:"Cuando alguien entre a la raíz (/), entrega manualmente el index.html".
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// 1. IMPORTANTE: Para que Express entienda el cuerpo de las peticiones POST
+// --- Middlewares ---
+// 1. Necesario para que la API pueda entender datos enviados en formato JSON (POST)
 app.use(express.json());
 // 2. Servir archivos estáticos (HTML/CSS)
 // app.use(express.static('public')); 
 
+// -- Controladores --
 const { comprobarDisponibilidad, finalizarPedido, obtenerCatalogoDisponible} = require('./controllers/pedidoController');
 
 // Endpoint de finalizar y guardar pedido
@@ -29,8 +34,13 @@ app.get('/api/catalogo', obtenerCatalogoDisponible)
 
 /* app.listen(3000, () => console.log('Servidor en puerto 3000')); */
 
+// --- Ejecución Dual (Local vs. Nube) ---
+// La condición 'require.main === module' verifica si el archivo se está ejecutando
+// directamente (node app.js). Si es así, inicia el servidor en el puerto 3000.
+// Si no, simplemente exportamos la app (module.exports) para que Vercel la tome y la gestione.
 if (require.main === module) {
     app.listen(3000, () => console.log('Servidor local en puerto 3000'));
 }
 
+// Exportamos la instancia de la app para que el archivo api/index.js de Vercel la use
 module.exports = app;
